b25c79d948a2753d9f08112c5b110c77bca0a26e
Damarice <menganyidamarice@gmail.com>
2023-05-11 15:13:15 +0300

    National Park Basic Functionality Updated

--- tourism/lib/National Parks.dart
+++ tourism/lib/National Parks.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class NationalPark extends StatelessWidget {
-  const NationalPark({super.key});
+  const NationalPark({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('National Park'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Our National Parks',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Do something
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Do something
+              },
+            ),
+            ListTile(
+              title: const Text('Item 3'),
+              onTap: () {
+                // Do something
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
